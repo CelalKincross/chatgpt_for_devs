7dae708d5b727b2b0cddcdb899a6f1030fdda102
W. Yang-Lee Young <celalkincross@gmail.com>
2023-05-17 09:07:08 +0800

    add guidelines, iterative techniques, and summarizing with LLMs

---
+++ Iterative.py
@@ -0,0 +1,15 @@
+################
+# Iterative Prompt Dev
+# 1. Idea
+# 2. Implementation
+# 3. Experimental Result
+# 4. Error Analysis
+# 5. Correct/ Clarify - Repeat
+#
+# After completing your prompt you may need to compare its 
+# performance with more examples i.e. introduce more fact sheets 
+# as in the example above 
+#
+#
+##################
+
