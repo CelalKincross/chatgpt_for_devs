585c1de704ac879e9c3d08bd1d57bf820439a208
W. Yang-Lee Young <celalkincross@gmail.com>
2023-05-20 18:43:15 +0800

    add expanding unit

---
+++ expanding.py
@@ -0,0 +1,41 @@
+# Expanding - taking short texts and generating longer, expanded versions
+# Generate customer service emails 
+# that are tailored to each customer's review
+
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+def get_completion(prompt, model="gpt-3.5-turbo",temperature=0): # Andrew mentioned that the prompt/ completion paradigm is preferable for this class
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+
+#### Adjusted Temperature- for more creative responses
+
+rompt = f"""
+You are a customer service AI assistant.
+Your task is to send an email reply to a valued customer.
+Given the customer email delimited by ```, \
+Generate a reply to thank the customer for their review.
+If the sentiment is positive or neutral, thank them for \
+their review.
+If the sentiment is negative, apologize and suggest that \
+they can reach out to customer service. 
+Make sure to use specific details from the review.
+Write in a concise and professional tone.
+Sign the email as `AI customer agent`.
+Customer review: ```{review}```
+Review sentiment: {sentiment}
+"""
+response = get_completion(prompt, temperature=0.7)
+print(response)
